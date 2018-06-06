8deda074a330b01640ab10b9cfed7e8ff38eae64
xarkes <antide.petit@gmail.com>
2018-06-06 14:38:18 +0200

    Added basic installation

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ framework radare2.
 This tool is still a work in progress.
 
 ## Package example
-```json
+```javascript
 {
 "name": "swf2",
 "type": "git",
--- r2pm.go
+++ r2pm.go
@@ -2,6 +2,7 @@ package main
 
 import "flag"
 import "fmt"
+import "strings"
 import "os"
 import "os/exec"
 import "runtime"
@@ -19,7 +20,7 @@ var R2PM_GITDIR string
 var R2PM_DB string
 var DBFILE string
 
-/* Utils functions */
+/* General util functions */
 func getenv(key, fallback string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
@@ -43,7 +44,34 @@ func isDirectory(path string) (bool, error) {
 	return fileInfo.IsDir(), err
 }
 
-/* R2PM functions */
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func gitClone(repoPath string, repoUrl string, args ...string) {
+	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
+		os.Chdir(R2PM_GITDIR)
+		fmt.Println("Downloading repository...")
+		cmdArgs := []string{"clone", repoUrl} // + args
+		cmd := exec.Command("git", cmdArgs...)
+		_, err := cmd.Output()
+		check(err)
+		fmt.Println("Download complete.")
+	} else {
+		fmt.Println("Repository already downloaded, updating...")
+		os.Chdir(repoPath)
+		cmd := exec.Command("git", "pull")
+		_, err := cmd.Output()
+		check(err)
+	}
+}
+
+/* R2PM utils */
 type PackageInfo struct {
 	Name      string   `json:"name"`
 	Type      string   `json:"type"`
@@ -53,6 +81,34 @@ type PackageInfo struct {
 	Uninstall []string `json:"uninstall"`
 }
 
+func getPackagesList() []string {
+	dat, err := ioutil.ReadFile(DBFILE)
+	check(err)
+	var packagesList []string
+	json.Unmarshal(dat, &packagesList)
+	return packagesList
+}
+
+func getPackageInfo(pkg string) (PackageInfo, error) {
+	var file string
+	// If the name contains a file separator, it's surely the full path
+	idx := strings.Index(pkg, string(os.PathSeparator))
+	if idx >= 0 {
+		file = pkg
+	} else {
+		file = path.Join(R2PM_DB, pkg)
+	}
+	dat, err := ioutil.ReadFile(file)
+	check(err)
+	pinfo := PackageInfo{}
+	err = json.Unmarshal([]byte(dat), &pinfo)
+	if err != nil {
+		return PackageInfo{}, err
+	}
+	return pinfo, err
+}
+
+/* R2PM functions */
 func r2pmInit() {
 	// Make sure git directory exists
 	if _, err := os.Stat(R2PM_GITDIR); os.IsNotExist(err) {
@@ -60,43 +116,28 @@ func r2pmInit() {
 	}
 
 	// Check if radare2-pm was already cloned
-	repo := path.Join(R2PM_GITDIR, "radare2-pm")
-	if _, err := os.Stat(repo); os.IsNotExist(err) {
-		os.Chdir(R2PM_GITDIR)
-		fmt.Println("Downloading repository...")
-		cmd := exec.Command("git", "clone", "https://github.com/radare/radare2-pm", "--depth=3", "--recursive")
-		_, err := cmd.Output()
-		check(err)
-		fmt.Println("Download complete.")
-	} else {
-		fmt.Println("Repository already downloaded, updating...")
-		os.Chdir(repo)
-		cmd := exec.Command("git", "pull")
-		_, err := cmd.Output()
-		check(err)
-	}
+	repoPath := path.Join(R2PM_GITDIR, "radare2-pm")
+	repoUrl := "https://github.com/radare/radare2-pm"
+	gitClone(repoPath, repoUrl, "--depth=3", "--recursive")
 
 	// Initialize database
-	packageRepo := path.Join(repo, "db")
+	packageRepo := path.Join(R2PM_DB, "db")
 	var validPackages []string
-	err := filepath.Walk(packageRepo, func(path string, info os.FileInfo, err error) error {
-		dir, _ := isDirectory(path)
+	err := filepath.Walk(packageRepo, func(file string, info os.FileInfo, err error) error {
+		dir, _ := isDirectory(file)
 		if dir {
 			return nil
 		}
 
 		// Read file content and parse it
-		dat, err := ioutil.ReadFile(path)
-		check(err)
-		pinfo := PackageInfo{}
-		err = json.Unmarshal([]byte(dat), &pinfo)
+		pinfo, err := getPackageInfo(file)
 		if err != nil {
 			return nil
 		}
 
 		// Validate package
-		if pinfo.Name != filepath.Base(path) {
-			fmt.Println("Invalid package name in '" + path + "': '" + pinfo.Name + "'")
+		if pinfo.Name != filepath.Base(file) {
+			fmt.Println("Invalid package name in '" + file + "': '" + pinfo.Name + "'")
 			return nil
 		}
 		validPackages = append(validPackages, pinfo.Name)
@@ -114,10 +155,7 @@ func r2pmInfo() {
 	fmt.Println("# Repository Database:")
 
 	// Read database file
-	dat, err := ioutil.ReadFile(DBFILE)
-	check(err)
-	var packagesList []string
-	json.Unmarshal(dat, &packagesList)
+	packagesList := getPackagesList()
 	packagesNumber := strconv.Itoa(len(packagesList))
 
 	fmt.Println("# " + packagesNumber + " Packages")
@@ -126,8 +164,41 @@ func r2pmInfo() {
 }
 
 func r2pmInstall(pkg string) bool {
-	fmt.Println("Installing " + pkg)
-	fmt.Println("TODO")
+	packagesList := getPackagesList()
+
+	// Check if the package is valid
+	if stringInSlice(pkg, packagesList) == false {
+		fmt.Println("Package " + pkg + " not found!")
+		return false
+	}
+
+	// Get package information
+	pinfo, err := getPackageInfo(pkg)
+	check(err)
+
+	// Download content
+	var newdir string
+	if pinfo.Type == "git" {
+		repoPath := path.Join(R2PM_GITDIR, filepath.Base(pinfo.Repo))
+		gitClone(repoPath, pinfo.Repo)
+	}
+	fmt.Println("Entering " + newdir)
+	os.Chdir(newdir)
+
+	// Install
+	fmt.Println("Installing " + pkg + "...")
+	for _, command := range pinfo.Install {
+		commandArgs := strings.Fields(command)
+		prog := commandArgs[0]
+		if prog == "cd" {
+			os.Chdir(commandArgs[1])
+			continue
+		}
+		commandArgs = commandArgs[1:]
+		cmd := exec.Command(prog, commandArgs...)
+		_, err := cmd.Output()
+		check(err)
+	}
 	return true
 }
 
