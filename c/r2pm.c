aead4a43a8a512dfed20a4a088c8ec578bce4cb8
Quentin Barrand <quentin@quba.fr>
2019-08-10 17:07:44 +0200

    Accept and return C values.

---
+++ c/r2pm.c
@@ -0,0 +1,10 @@
+#include "r2pm.h"
+
+int main() {
+    R2pmSetDebug(1);
+
+    char* path = "/home/quentin/.local/share/radare2/r2pm/";
+
+    R2pmInit(path);
+    R2pmDelete(path);
+}
--- lib/main.go
+++ lib/main.go
@@ -1,34 +1,64 @@
 package main
 
+import "C"
+
 import (
-	"C"
+	"log"
 
 	"github.com/radareorg/r2pm/internal/features"
 )
 
+var debug = false
+
+func getReturnValue(err error) C.int {
+	if err != nil {
+		if debug {
+			log.Print(err.Error())
+		}
+
+		return -1
+	}
+
+	return 0
+}
+
 //export R2pmDelete
-func R2pmDelete(r2pmDir string) error {
-	return features.Delete(r2pmDir)
+func R2pmDelete(r2pmDir *C.char) C.int {
+	err := features.Delete(C.GoString(r2pmDir))
+	return getReturnValue(err)
 }
 
 //export R2pmInit
-func R2pmInit(r2pmDir string) error {
-	return features.Init(r2pmDir)
+func R2pmInit(r2pmDir *C.char) C.int {
+	err := features.Init(C.GoString(r2pmDir))
+	return getReturnValue(err)
 }
 
 //export R2pmInstall
-func R2pmInstall(r2pmDir, packageName string) error {
-	return features.Install(r2pmDir, packageName)
+func R2pmInstall(r2pmDir, packageName *C.char) C.int {
+	err := features.Install(C.GoString(r2pmDir), C.GoString(packageName))
+	return getReturnValue(err)
 }
 
 //export R2pmList
-func R2pmList(r2pmDir string) ([]string, error) {
-	return features.List(r2pmDir)
+func R2pmList(r2pmDir *C.char) ([]string, C.int) {
+	entries, err := features.List(C.GoString(r2pmDir))
+	return entries, getReturnValue(err)
 }
 
 //export R2pmUninstall
-func R2pmUninstall(r2pmDir, packageName string) error {
-	return features.Uninstall(r2pmDir, packageName)
+func R2pmUninstall(r2pmDir, packageName *C.char) C.int {
+	err := features.Uninstall(C.GoString(r2pmDir), C.GoString(packageName))
+	return getReturnValue(err)
+}
+
+//export R2pmSetDebug
+func R2pmSetDebug(value C.int) {
+	debug = value != 0
+
+	if debug {
+		log.Print("debug enabled")
+	}
 }
 
 func main() {}
