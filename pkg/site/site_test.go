28b35abb61af1126d287ec5bcb330317724bd733
Quentin Barrand <quentin@quba.fr>
2019-08-13 22:41:44 +0200

    Add a test.

---
+++ pkg/site/site_test.go
@@ -0,0 +1,36 @@
+package site
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	siteDir := filepath.Join(tempDir, "r2pm")
+
+	s, err := New(siteDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedDirs := []string{
+		s.databaseSubDir(),
+		s.gitSubDir(),
+		s.installedSubDir(),
+	}
+
+	for _, ed := range expectedDirs {
+		t.Run(ed, func(t *testing.T) {
+			if info, err := os.Stat(ed); err != nil || !info.IsDir() {
+				t.Fail()
+			}
+		})
+	}
+}
