5099035f3f164a4f0b6e4e6bd5f5f6ebd283504c
Quentin Barrand <quentin.barrand@cern.ch>
2019-08-24 12:50:40 +0200

    Fix the BSD paths.

--- internal/util/dir/dir_test_freebsd.go
+++ internal/util/dir/dir_test_freebsd.go
@@ -3,7 +3,7 @@ package dir
 import "testing"
 
 func testSiteDir(t *testing.T) {
-	if s := SiteDir(); s != "/usr/share/RadareOrg/r2pm" {
+	if s := SiteDir(); s != "/usr/local/share/RadareOrg/r2pm" {
 		t.Fatal(s)
 	}
 }
